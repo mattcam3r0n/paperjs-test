406fcd5eeee7c5e8de3b2d6c8a74b3494bb0e127
Matt Cameron <cameron.matt@gmail.com>
2018-12-27 22:26:10 -0600

    path tool refactoring. line utils.

---
+++ src/lib/LineUtils.js
@@ -0,0 +1,96 @@
+import { Point } from 'paper';
+import { round } from 'lodash';
+import FieldDimensions from './FieldDimensions';
+
+export default class LineUtils {
+  static degreesInRadians(degrees) {
+    return (degrees * Math.PI) / 180;
+  }
+
+  static radiansInDegrees(radians) {
+    return (radians * 180) / Math.PI + 180;
+  }
+
+  static calculateAngle(p1, p2) {
+    const angle = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+
+    return this.radiansInDegrees(angle);
+  }
+
+  static snapToAngle(p1, p2) {
+    // TODO: find a way to do this mathematically
+    const sectorMap = {
+      0: 360,
+      1: 45,
+      2: 45,
+      3: 90,
+      4: 90,
+      5: 135,
+      6: 135,
+      7: 180,
+      8: 180,
+      9: 225,
+      10: 225,
+      11: 270,
+      12: 270,
+      13: 315,
+      14: 315,
+      15: 360,
+    };
+    let angle = this.calculateAngle(p1, p2) - 180;
+    angle = this.normalizeAngle(angle);
+    const sector = Math.floor(angle / 22.5);
+    const snapAngle = this.degreesInRadians(sectorMap[sector]);
+    return snapAngle;
+  }
+
+  static snapToLine(p1, p2) {
+    const v = p2.subtract(p1);
+    const a = this.radiansInDegrees(this.snapToAngle(p1, p2)) + 180;
+    if (a % 90 > 0) return this.snapToOblique(p1, p2);
+    v.angle = a; // snap angle
+    v.length = round(v.length);
+    const p = p1.add(v);
+    return new Point((p.x), (p.y));
+}
+
+  static snapToOblique(p1, p2) {
+    const v = p2.subtract(p1);
+    const a = this.radiansInDegrees(this.snapToAngle(p1, p2)) + 180;
+    v.angle = a; // snap angle
+    v.length = round(v.length / FieldDimensions.obliqueStepSize) * FieldDimensions.obliqueStepSize;
+    const p = p1.add(v);
+    return new Point((p.x), (p.y));
+  }
+
+  static calculateDistance(p1, p2) {
+    var a = p1.x - p2.x;
+    var b = p1.y - p2.y;
+
+    return Math.sqrt(a * a + b * b);
+  }
+
+  static normalizeAngle(a) {
+    if (a >= 0 && a < 360) return a;
+
+    return a - Math.floor(a / 360) * 360;
+  }
+
+  static getLengthInSteps(segment) {
+    if (this.isOblique(segment)) {
+      return round(segment.curve.length / FieldDimensions.obliqueStepSize);
+    }
+    return round(segment.curve.length);
+  }
+
+  static isOblique(segment) {
+    const angle = round(segment.curve.line.vector.angle + 90);
+    return angle % 90 !== 0;
+  }
+
+  static isVertical(segment) {
+    const angle = segment.curve.line.vector.angle + 90;
+    return angle === 0 || angle === 180;
+  }
+
+}
--- src/lib/PathTool.js
+++ src/lib/PathTool.js
@@ -1,6 +1,5 @@
-import paper, { Path, Point, PointText } from 'paper';
-import { round } from 'lodash';
-import FieldDimensions from './FieldDimensions';
+import paper, { Path, PointText } from 'paper';
+import LineUtils from './LineUtils';
 
 export default class PathTool {
   constructor(paperScope) {
@@ -12,27 +11,63 @@ export default class PathTool {
     this.tool.onMouseMove = this.onMouseMove;
   }
 
+  activate() {
+    this.tool.activate();
+  }
+
+  dispose() {
+    this.tool.off('mousedown');
+    this.tool.off('mousedrag');
+    this.tool.off('mousemove');
+    this.tool.remove();
+    this.tool = null;
+  }
+
   get cursor() {
     return 'crosshair';
   }
 
+  onMouseDown = (event) => {
+    const { item } = event;
+    const { path, marcher } = this;
+
+    if (path) {
+      path.add(event.point);
+      return;
+    }
+
+    if (item && item._itemType === 'marcher') {
+      this.path = new Path({
+        strokeColor: 'gray',
+        strokeWidth: 0.25,
+        strokeCap: 'round',
+        dashArray: [0.5, 0.5],
+        segments: [marcher.position, marcher.position],
+        selected: true
+      });
+    }
+  };
+
+  onMouseDrag = (event) => {};
+
   onMouseMove = (event) => {
     const { item } = event;
+    const { path } = this;
 
     // ignore mouse moves that originate from other than the canvas
     const targetNodeName = event.event.target.nodeName;
-    if (targetNodeName !== "CANVAS") {
+    if (targetNodeName !== 'CANVAS') {
       this.hideLastSegment();
       return;
     }
 
-    if (this.path) {
-      const snapped = this.snapToLine(
-        this.path.lastSegment.previous.point,
+    if (path) {
+      const snapped = LineUtils.snapToLine(
+        path.lastSegment.previous.point,
         event.point
       );
-      this.path.lastSegment.point = snapped;
-      this.drawPathSegmentLength(this.path.lastSegment);
+      path.lastSegment.point = snapped;
+      this.drawPathSegmentLength(path.lastSegment);
     }
 
     if (item && item._itemType === 'marcher') {
@@ -56,40 +91,26 @@ export default class PathTool {
     const p2 = segment.previous.point;
     const v = p2.subtract(p1).divide(2);
     let textPoint = p1.add(v);
-    if (this.isVertical(segment)) { // place to left of vertical line
+    if (LineUtils.isVertical(segment)) {
+      // place to left of vertical line
       textPoint = textPoint.add({ x: -1.5, y: 0 });
     } else {
       textPoint = textPoint.add({ x: 0, y: -0.5 });
     }
-    const text = segment.pathSegmentLength || new PointText({
-      point: textPoint,
-      fontFamily: 'Courier New',
-      fontWeight: 'bold',
-      fillColor: 'deepskyblue',
-      justification: 'center',
-      fontSize: 2
-    });
-    text.content = this.getLengthInSteps(segment);
+    const text =
+      segment.pathSegmentLength ||
+      new PointText({
+        point: textPoint,
+        fontFamily: 'Courier New',
+        fontWeight: 'bold',
+        fillColor: 'deepskyblue',
+        justification: 'center',
+        fontSize: 2,
+      });
+    text.content = LineUtils.getLengthInSteps(segment);
     text.point = textPoint;
     text.visible = true;
     segment.pathSegmentLength = text;
-  }
-
-  getLengthInSteps(segment) {
-    if (this.isOblique(segment)) {
-      return round(segment.curve.length / FieldDimensions.obliqueStepSize);
-    }
-    return round(segment.curve.length);
-  }
-
-  isOblique(segment) {
-    const angle = round(segment.curve.line.vector.angle + 90);
-    return angle % 90 !== 0;
-  }
-
-  isVertical(segment) {
-    const angle = segment.curve.line.vector.angle + 90;
-    return angle === 0 || angle === 180;
   }
 
   highlightMarcher = (item) => {
@@ -104,113 +125,4 @@ export default class PathTool {
     this.marcher.strokeColor = 'black';
     this.marcher = null;
   };
-
-  onMouseDown = (event) => {
-    const { item } = event;
-
-    if (this.path) {
-      this.path.add(event.point);
-      console.log('lastCurve', this.path.lastCurve);
-      return;
-    }
-
-    if (item && item._itemType === 'marcher') {
-      this.path = new Path();
-      this.path.strokeColor = 'gray';
-      this.path.strokeWidth = 0.25;
-      this.path.dashArray = [0.5, 0.5];
-      this.path.strokeCap = 'round';
-      this.path.add(this.marcher.position);
-      this.path.add(this.marcher.position);
-      this.path.selected = true;
-    }
-  };
-
-  onMouseDrag = (event) => {};
-
-  activate() {
-    this.tool.activate();
-  }
-
-  dispose() {
-    this.tool.off('mousedown');
-    this.tool.off('mousedrag');
-    this.tool.off('mousemove');
-    this.tool.remove();
-    this.tool = null;
-  }
-
-  // TODO: move to utils class
-  degreesInRadians(degrees) {
-    return (degrees * Math.PI) / 180;
-  }
-
-  radiansInDegrees(radians) {
-    return (radians * 180) / Math.PI + 180;
-  }
-
-  calculateAngle(p1, p2) {
-    const angle = Math.atan2(p2.y - p1.y, p2.x - p1.x);
-
-    return this.radiansInDegrees(angle);
-  }
-
-  snapToAngle(p1, p2) {
-    // TODO: find a way to do this mathematically
-    const sectorMap = {
-      0: 360,
-      1: 45,
-      2: 45,
-      3: 90,
-      4: 90,
-      5: 135,
-      6: 135,
-      7: 180,
-      8: 180,
-      9: 225,
-      10: 225,
-      11: 270,
-      12: 270,
-      13: 315,
-      14: 315,
-      15: 360,
-    };
-    let angle = this.calculateAngle(p1, p2) - 180;
-    angle = this.normalizeAngle(angle);
-    const sector = Math.floor(angle / 22.5);
-    const snapAngle = this.degreesInRadians(sectorMap[sector]);
-    return snapAngle;
-  }
-
-  snapToLine(p1, p2) {
-    const v = p2.subtract(p1);
-    const a = this.radiansInDegrees(this.snapToAngle(p1, p2)) + 180;
-    if (a % 90 > 0) return this.snapToOblique(p1, p2);
-    v.angle = a; // snap angle
-    v.length = round(v.length);
-    const p = p1.add(v);
-    return new Point((p.x), (p.y));
-}
-
-  snapToOblique(p1, p2) {
-    const v = p2.subtract(p1);
-    const a = this.radiansInDegrees(this.snapToAngle(p1, p2)) + 180;
-    v.angle = a; // snap angle
-    v.length = round(v.length / FieldDimensions.obliqueStepSize) * FieldDimensions.obliqueStepSize;
-    const p = p1.add(v);
-    return new Point((p.x), (p.y));
-  }
-
-  calculateDistance(p1, p2) {
-    var a = p1.x - p2.x;
-    var b = p1.y - p2.y;
-
-    return Math.sqrt(a * a + b * b);
-  }
-
-  normalizeAngle(a) {
-    if (a >= 0 && a < 360) return a;
-
-    return a - Math.floor(a / 360) * 360;
-  }
 }
