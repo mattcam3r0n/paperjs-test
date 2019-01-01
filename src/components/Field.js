05013f91cf018a972edd9bbfcaee7e770360f9bf
Matt Cameron <cameron.matt@gmail.com>
2018-12-31 21:51:55 -0600

    FieldController

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,30 +1,12 @@
 import React, { Component } from 'react';
-import { reaction } from 'mobx';
 import { observer, inject } from 'mobx-react';
-import paper from 'paper';
-//import { compose } from 'recompose';
-
-import FieldPainter from '../lib/FieldPainter';
 import FieldController from '../lib/FieldController';
 
 @inject('designViewState')
 @observer
 class Field extends Component {
+
   controller;
-
-  onResize = () => {
-    if (!this.fieldPainter) return;
-    const { designViewState } = this.props;
-    const fieldContainer = document.getElementById('fieldContainer');
-    const { clientWidth, clientHeight } = fieldContainer;
-    this.fieldPainter.resize(clientWidth, clientHeight);
-
-    designViewState.setFieldContainerSize({
-      width: clientWidth,
-      height: clientHeight
-    });
-    designViewState.zoomToFit();
-  };
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.onResize);
@@ -35,43 +17,18 @@ class Field extends Component {
 
     window.addEventListener('resize', this.onResize);
 
-    // move these to init/wireup funcs?
-    reaction(() => designViewState.drill,
-    (drill, reaction) => {
-      this.fieldPainter.syncMarchers(drill);
-    });
+    const canvas = document.getElementById('fieldCanvas');
+    this.controller = new FieldController(canvas, designViewState);
 
-    reaction(
-      () => designViewState.zoomFactor,
-      (zoomFactor, reaction) => {
-        this.fieldPainter.zoom(zoomFactor);
-      }
-    );
-
-    reaction(
-      () => designViewState.center,
-      (center, reaction) => {
-        this.fieldPainter.setCenter(center);
-      }
-    );
-
-    this.drawField();
     this.onResize();
     designViewState.fieldInitialized();
   }
 
-  drawField() {
-    const { designViewState } = this.props;
-    const canvas = document.getElementById('fieldCanvas');
-    // create paperscope
-    const paperScope = new paper.PaperScope();
-    paperScope.setup(canvas);
-    designViewState.setFieldPaperScope(paperScope);
-    // create field painter
-    this.fieldPainter = new FieldPainter(paperScope);
-    this.fieldPainter.draw();
-    this.onResize();
-  }
+  onResize = () => {
+    if (!this.controller) return;
+    const fieldContainer = document.getElementById('fieldContainer');
+    this.controller.resize(fieldContainer.clientWidth, fieldContainer.clientHeight);
+  };
 
   render() {
     const { designViewState } = this.props;
@@ -81,15 +38,10 @@ class Field extends Component {
         style={{
           flex: 'auto',
           overflow: 'hidden',
-          // backgroundColor: 'gray',
-          //   width: 'calc(100vw - 240px)',
-          //   height: 'calc((100vw - 240px) / 2)',
         }}
       >
         <canvas
           id="fieldCanvas"
-          //   data-paper-resize="true"
-          // style={{ width: '100%', height: '100%', border: 'solid 1px black' }}
           style={{ cursor: designViewState.activeTool  ? designViewState.activeTool.cursor : 'default'}}
         />
       </div>
--- src/lib/FieldController.js
+++ src/lib/FieldController.js
@@ -1,10 +1,58 @@
-export default class FieldController {
-    constructor(canvas, designViewState) {
-        this.canvas = canvas; // do I need to keep this?
-        this.viewState = designViewState;
+import { reaction } from 'mobx';
+import paper from 'paper';
+import FieldPainter from '../lib/FieldPainter';
 
-        // create paperscope
-        // initial draw & resize
-        // wire up reactions
-    }
+export default class FieldController {
+  constructor(canvas, designViewState) {
+    this.canvas = canvas; // do I need to keep this?
+    this.viewState = designViewState;
+    // init paper js
+    this.initializePaperScope(canvas);
+    // initial draw
+    this.fieldPainter = new FieldPainter(this.viewState.fieldPaperScope);
+    this.fieldPainter.draw();
+    // wire up reactions to respond to state changes
+    this.configureReactions();
+  }
+
+  initializePaperScope(canvas) {
+    const paperScope = new paper.PaperScope();
+    paperScope.setup(canvas);
+    this.viewState.setFieldPaperScope(paperScope);
+  }
+
+  configureReactions() {
+    // drill changed
+    reaction(
+      () => this.viewState.drill,
+      (drill, reaction) => {
+        this.fieldPainter.syncMarchers(drill);
+      }
+    );
+
+    // change zoom
+    reaction(
+      () => this.viewState.zoomFactor,
+      (zoomFactor, reaction) => {
+        this.fieldPainter.zoom(zoomFactor);
+      }
+    );
+
+    // change center
+    reaction(
+      () => this.viewState.center,
+      (center, reaction) => {
+        this.fieldPainter.setCenter(center);
+      }
+    );
+  }
+
+  resize(width, height) {
+    this.fieldPainter.resize(width, height);
+    this.viewState.setFieldContainerSize({
+      width: width,
+      height: height,
+    });
+    this.viewState.zoomToFit();
+  }
 }
