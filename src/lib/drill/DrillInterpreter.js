c72d5b4e8f8110f591b6e8e6436f2e39c82752cd
Matt Cameron <cameron.matt@gmail.com>
2019-04-09 11:09:12 -0500

    DrillInterpreter stepForward, tests

--- src/lib/drill/DrillInterpreter.js
+++ src/lib/drill/DrillInterpreter.js
@@ -1,9 +1,19 @@
+import ScriptInterpreter from "./ScriptInterpreter";
+
 export default class DrillInterpreter {
   constructor(drill) {
       this.drill = drill;
+      this.scriptInterpreter = new ScriptInterpreter();
+      this.count = 0;
   }
 
-  stepForward() {}
+  stepForward() {
+    const { marchers } = this.drill;
+    marchers.forEach(m => {
+      m.script.currentState = this.scriptInterpreter.stepForward(m.script);
+      this.count++;
+    });
+  }
 
   stepBackward() {}
 
--- src/lib/drill/DrillInterpreter.test.js
+++ src/lib/drill/DrillInterpreter.test.js
@@ -1,5 +1,28 @@
 import DrillInterpreter from './DrillInterpreter';
+import BlockBuilder from './BlockBuilder';
 
-// describe('DrillInterpreter', () => {
-// });
+describe('DrillInterpreter', () => {
+  describe('stepForward', () => {
+    test('all marchers advance', () => {
+      const builder = new BlockBuilder();
+      const block = builder
+        .createBlock()
+        .addStepsFromString('EE')
+        .build();
+      const drill = {
+        marchers: block,
+      };
+      const drillInterpreter = new DrillInterpreter(drill);
+      drillInterpreter.stepForward();
+      drillInterpreter.stepForward();
 
+      // moved two steps E.  y should not change.
+      drill.marchers.forEach((m) => {
+        const initial = m.script.initialState.position;
+        const current = m.script.currentState.position;
+        expect(current.x).toEqual(initial.x + 2);
+        expect(current.y).toEqual(initial.y);
+      });
+    });
+  });
+});
