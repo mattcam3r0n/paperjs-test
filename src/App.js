948186fdab2843975b767086fec6bd0d82515ab2
Matt Cameron <cameron.matt@gmail.com>
2019-04-13 15:00:32 -0500

    rough out authenticated routes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import {
-  withStyles,
-  MuiThemeProvider,
-} from '@material-ui/core/styles';
+import { withStyles, MuiThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import { styles, theme } from './App.styles';
 
@@ -21,45 +18,59 @@ import DesignView from './components/DesignView';
 
 Amplify.configure(awsmobile);
 
-
+// TODO: replace with real components
 const Home = () => <h1>Home</h1>;
-const Login = () => <h1>Login</h1>;
+const Login = () => <Authenticator hide={[Greetings]} />;
+const About = () => <h1>About</h1>;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     Hub.listen('auth', (data) => {
-      const { payload } = data;
-      this.onAuthEvent(payload);
+      this.onAuthEvent(data.payload);
     });
   }
 
+  state = {
+    user: null,
+  };
+
+  componentDidMount() {
+    // Analytics.record('quiz-app mounted.');
+    this.loadUser();
+  }
+
   onAuthEvent(payload) {
-    console.log('A new auth event has happened: ', payload.data.username + ' has ' + payload.event);
+    console.log('payload', payload);
+    console.log(
+      'A new auth event has happened: ',
+      payload.data.username + ' has ' + payload.event
+    );
+    this.loadUser();
+  }
+
+  loadUser() {
+    Auth.currentAuthenticatedUser()
+      .then((user) => this.setState({ user: user }))
+      .catch(() => this.setState({ user: null }));
   }
 
   render() {
     const { classes } = this.props;
-
-    //console.log('theme', theme);
+    const { user } = this.state;
 
     return (
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
         <Router>
-        <div className={classes.root}>
-          <Header />
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/design" component={DesignView} />
-          {/* 
-          <PrivateRoute
-            path="/about"
-            user={user}
-            component={About}
-          /> 
-          */}
-        </div>
+          <div className={classes.root}>
+            <Header />
+            <Route exact path="/" component={Home} />
+            <Route path="/home" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/design" component={DesignView} />
+            <PrivateRoute path="/about" user={user} component={About} />
+          </div>
         </Router>
       </MuiThemeProvider>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
 import { withStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -14,33 +15,44 @@ const styles = (theme) => ({
     backgroundColor: 'black',
   },
   appName: {
-    marginRight: 32
+    marginRight: 32,
   },
 });
 
 @inject('designViewState')
 @observer
 class Header extends React.Component {
-  state = {
-  };
+  state = {};
 
   render() {
     const { classes, designViewState } = this.props;
-    const { anchorEl } = this.state;
+    // const { anchorEl } = this.state;
     return (
       <AppBar position="absolute" className={classes.appBar}>
         <Toolbar variant="dense">
-          <Typography variant="title" color="inherit" noWrap className={classes.appName}>
+          <Typography
+            variant="title"
+            color="inherit"
+            noWrap
+            className={classes.appName}
+            component={Link}
+            to="/home"
+          >
             Precision
           </Typography>
           <DropDownMenu menuText="File">
-            <DropDownMenuItem onClick={designViewState.newDrill}>New Drill</DropDownMenuItem>
+            <DropDownMenuItem onClick={designViewState.newDrill}>
+              New Drill
+            </DropDownMenuItem>
             <DropDownMenuItem>Open...</DropDownMenuItem>
           </DropDownMenu>
           <DropDownMenu menuText="Edit">
             <DropDownMenuItem>Undo</DropDownMenuItem>
             <DropDownMenuItem>Redo</DropDownMenuItem>
           </DropDownMenu>
+          <Button color="inherit" component={Link} to="/about">
+            About
+          </Button>
         </Toolbar>
       </AppBar>
     );
